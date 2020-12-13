46da11eee8143169aefb956511036dde529de3b9
JakubKurczak <jakubkurczak97@gmail.com>
2020-12-13 11:40:17 +0100

    change PDO to mysqli

--- php/db_ops/db_connection.php
+++ php/db_ops/db_connection.php
@@ -1,6 +1,6 @@
 <?php
 const DB_CONF = array(
-    'servername' => '127.0.0.1',
+    'servername' => '172.26.160.1',
     'login' => 'root',
     'password' => 'password',
     'db_name' => 'piekarnia',
@@ -9,24 +9,25 @@ const DB_CONF = array(
 
 function get_connetion()
 {
-    try {
-        $connection = new PDO('mysql:host=' . DB_CONF["servername"] . ';dbname=' . DB_CONF["db_name"] . ';port=' . DB_CONF["port"], DB_CONF['login'], DB_CONF['password']);
-        $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        return $connection;
-    } catch (PDOException $e) {
-        echo $e->getMessage();
-        return NULL;
+
+    $connection = new mysqli(DB_CONF['servername'], DB_CONF['login'], DB_CONF['password'], DB_CONF['db_name'], DB_CONF['port']);
+    if ($connection->connect_errno) {
+        echo "Failed to connect to MySQL: " . $connection->connect_error;
+        exit();
     }
+    return $connection;
 }
 
 function get_password($user_mail)
 {
     $connection = get_connetion();
     if ($connection != NULL) {
-        $statement = $connection->prepare('SELECT password FROM users WHERE users.email="' . $user_mail . '"');
-        $statement->execute();
-        $row = $statement->fetch();
-        return $row['password'];
+        $result = $connection->query('SELECT password FROM users WHERE users.email="' . $user_mail . '"');
+        if ($result->num_rows > 0) {
+            $row = $result->fetch_assoc();
+            return $row['password'];
+        } else
+            return NULL;
     } else {
         return NULL;
     }
@@ -36,10 +37,12 @@ function get_id($user_mail)
 {
     $connection = get_connetion();
     if ($connection != NULL) {
-        $statement = $connection->prepare('SELECT id FROM users WHERE users.email="' . $user_mail . '"');
-        $statement->execute();
-        $row = $statement->fetch();
-        return $row['id'];
+        $result = $connection->query('SELECT id FROM users WHERE users.email="' . $user_mail . '"');
+        if ($result->num_rows > 0) {
+            $row = $result->fetch_assoc();
+            return $row['id'];
+        } else
+            return NULL;
     } else {
         return NULL;
     }
@@ -49,21 +52,26 @@ function get_id_from_login($user_login)
 {
     $connection = get_connetion();
     if ($connection != NULL) {
-        $statement = $connection->prepare('SELECT id FROM users WHERE users.login="' . $user_login . '"');
-        $statement->execute();
-        $row = $statement->fetch();
-        return $row['id'];
+        $result = $connection->query('SELECT id FROM users WHERE users.login="' . $user_login . '"');
+        if ($result->num_rows > 0) {
+            $row = $result->fetch_assoc();
+            return $row['id'];
+        } else
+            return NULL;
     } else {
         return NULL;
     }
 }
+
 function get_orders($user_login)
 {
     $connection = get_connetion();
+    $result_array = array();
     if ($connection != NULL) {
-        $statement = $connection->prepare('SELECT * FROM orders WHERE orders.usersid=' . get_id_from_login($user_login));
-        $statement->execute();
-        return $statement->fetchAll();
+        $result = $connection->query('SELECT * FROM orders WHERE orders.usersid=' . get_id_from_login($user_login));
+        while( $row = $result->fetch_assoc())
+            array_push($result_array, $row);
+        return $result_array;
     } else
         return NULL;
 }
